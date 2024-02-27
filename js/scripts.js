ddc8a7d0d7ca626bd1f6025d985c802696ea65a8
Gaston Ojeda <gaston.ojeda@hotmail.com.ar>
2024-02-27 15:59:32 -0300

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -37,28 +37,6 @@ window.addEventListener('DOMContentLoaded', event => {
             rootMargin: '0px 0px -40%',
         });
     };
-
-    // Back to top button
-    let mybutton = document.getElementById("btn-back-to-top");
-
-    window.onscroll = function () {
-        scrollFunction();
-    };
-
-    function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            mybutton.style.display = "block";
-        } else {
-            mybutton.style.display = "none";
-        }
-    }
-
-    mybutton.addEventListener("click", backToTop);
-
-    function backToTop() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-    }
 });
 
 // Hide navbar on responsive
